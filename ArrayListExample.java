089d8833a695e3989f82fc6ceb5fb0baf55fbf14
Divya123solanki <solankidivya817@gmail.com>
2024-08-06 12:50:07 +0530

    Add files via upload

---
+++ ArrayListExample.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class ArrayListExample {
+    public static void main(String args[]){
+        ArrayList<String> l=new ArrayList();
+        l.add("java");
+        l.add("python");
+        l.add("PHP");
+        l.add("c");
+        //l.removeAll()
+        //l.add(2,"golang");
+       // l.remove(1);
+     //  l.contains("java");
+    l.clear();
+    //l.indexOf(3);
+        System.out.println(l);
+    }
+}

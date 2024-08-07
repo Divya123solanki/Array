8476273de673eb3de02f67874bb1ee966a2c19d4
Divya123solanki <solankidivya817@gmail.com>
2024-08-07 10:34:41 +0530

    Add files via upload

---
+++ Array6.java
@@ -0,0 +1,17 @@
+ class Array6 {
+    public static void main(String args[]){
+        int a[]={10,20,30,40,50,60};
+        int delete=30;
+        for(int i=0;i<a.length;i++){
+            if(delete==a[i]){
+                for(int j=i;j<a.length-1;j++){
+                    a[j]=a[j+1];
+                }
+                break;
+            }
+        }
+        for(int i=0;i<a.length-1;i++){
+            System.out.print(" "+a[i]);
+        }
+    }
+}

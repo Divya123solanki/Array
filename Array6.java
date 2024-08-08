7719468c3a9971b3e4f65af0cce909793509a655
Divya123solanki <solankidivya817@gmail.com>
2024-08-07 21:09:25 -0400

    Add files via upload

--- Array6.java
+++ Array6.java
@@ -1,17 +1,19 @@
+ //sort the array element
  class Array6 {
     public static void main(String args[]){
-        int a[]={10,20,30,40,50,60};
-        int delete=30;
+        int a[]={6,8,2,4,3,1,5,7,9};
+        int temp;
         for(int i=0;i<a.length;i++){
-            if(delete==a[i]){
-                for(int j=i;j<a.length-1;j++){
-                    a[j]=a[j+1];
+            for(int j=i+1;j<a.length;j++){
+                if(a[i]<a[j]){
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
                 }
-                break;
             }
         }
-        for(int i=0;i<a.length-1;i++){
-            System.out.print(" "+a[i]);
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
         }
     }
 }

5a90cb43682b07325b67e87f9527c8b94d016e12
Divya123solanki <solankidivya817@gmail.com>
2024-08-09 02:49:13 -0400

    Add files via upload

---
+++ Array11.java
@@ -0,0 +1,20 @@
+// Intersection of the array
+class Array11 {
+    public static void main(String args[]){
+        int a[]={4,3,7,9,2};
+        int b[]={5,1,4,8,3,7,9};
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<b.length;j++){
+                if(a[i]==b[j]){
+                   System.out.print(a[i]+" , ");
+                }
+                else{
+
+                }
+            }
+          
+            
+        }
+      
+    }
+}

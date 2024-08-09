b8fc4ec6c918d335106489cd1be702078caac7f7
Divya123solanki <solankidivya817@gmail.com>
2024-08-09 02:48:04 -0400

    Add files via upload

--- Array8.java
+++ Array8.java
@@ -1,17 +1,28 @@
- //Find the occurance of an integer number in array
+ //Find pick element of the array 
  class Array8 {
     public static void main(String args[]){
-        int a[]={1,2,3,4,2,2,2,2,2,2};
-        int count=0;
-        int n=2;
-        for(int i=0;i<a.length;i++){
-            if(n==a[i]){
-                count++;
-         
+        int a[]={3,6,8,2,4,9,6};
+        int n=a.length;
+        if(n==1){
+            System.out.print(a[0]);
+        }
+      else if(a[0]>a[1]){
+        System.out.print(a[0]);
+        
+       }
+       else if(a[a.length-1]>a[a.length-2]){
+        System.out.print(a[a.length-1]);
+        
+       }
+       else{
+        for(int i=1;i<a.length-1;i++){
+            if(a[i]>a[i-1]&&a[i]>a[i+1]){
+                System.out.print(a[i]);
+                break;
             }
-            //System.out.print(count);
-}
-System.out.print(count);
- }
-}
+           
+        }
+    }
 
+    }
+}

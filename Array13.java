4de559bec1f4ccfe8d2259d2be7bfa8a5cc4f570
Divya123solanki <solankidivya817@gmail.com>
2024-08-10 12:06:02 -0400

    Add files via upload

---
+++ Array13.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Array13 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int i,n;
+        System.out.println("Enter the number of elements");
+        n=sc.nextInt();
+        int[] a=new int[n];
+        int[] reverse = new int[n];
+        System.out.println("enter the elements");
+        for(i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        for ( i = 0; i < n; i++) {
+            reverse[i]=a[n-i-1];
+        }
+        System.out.println("Elements in reverse order are");
+         for ( i = 0; i < n; i++) {
+             System.out.print(reverse[i]+" ");
+         }
+    }
+}

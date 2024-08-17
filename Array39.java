b77fb8de355bfe4f0d37e459746a2f0ef3365176
Divya123solanki <solankidivya817@gmail.com>
2024-08-16 22:13:51 -0400

    Add files via upload

---
+++ Array39.java
@@ -0,0 +1,11 @@
+//find the odd no in array
+class Array39 {
+    public static void main(String args[]){
+        int a[]={1,3,4,5,6,8};
+        for(int i=0;i<a.length;i++){
+            if(a[i]%2!=0){
+                System.out.println(a[i]);
+            }
+        }
+    }
+}

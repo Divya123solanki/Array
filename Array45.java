e655ca6999433d22125d16fa3033d3c0abf74edc
Divya123solanki <solankidivya817@gmail.com>
2024-08-18 10:37:42 -0400

    Add files via upload

---
+++ Array45.java
@@ -0,0 +1,15 @@
+ //Find the first repeating element
+ class Array45 {
+    public static void main(String args[]){
+       int a[]={6,5,1,2,1,3,2,6};
+       for(int i=0;i<a.length;i++){
+        for(int j=i+1;j<a.length;j++){
+            if(a[i]==a[j]){
+                System.out.println(a[i]+" ");
+            }
+            }
+            break;
+        }
+       }
+    }
+

7abb5b5a7ab81cf69e8d5298caa82310aaf57311
Divya123solanki <solankidivya817@gmail.com>
2024-08-18 10:38:02 -0400

    Add files via upload

---
+++ Array46.java
@@ -0,0 +1,14 @@
+ //Find repeating element
+ class Array46 {
+    public static void main(String args[]){
+        int a[]={6,5,1,2,1,3,2,6};
+        for(int i=0;i<a.length;i++){
+         for(int j=i+1;j<a.length;j++){
+             if(a[i]==a[j]){
+                 System.out.println(a[i]+" ");
+             }
+             }
+            
+         }
+    }
+}

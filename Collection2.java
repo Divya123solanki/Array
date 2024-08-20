76840a64eb79693e8f27e1cc50faeb6086dbf910
Divya123solanki <solankidivya817@gmail.com>
2024-08-20 10:45:18 -0400

    Add files via upload

---
+++ Collection2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Collection2 {
+    public static void main(String args[]){
+        ArrayList a=new ArrayList();
+        a.add(100);
+        a.add(200);
+        a.add(300);
+        System.out.print(a.contains(200));
+    }
+}

3a8ccfebf5ca4c922ce63a787686681c704617ef
Divya123solanki <solankidivya817@gmail.com>
2024-08-22 21:42:02 -0400

    Add files via upload

---
+++ String5.java
@@ -0,0 +1,10 @@
+//Reverse the Sting
+class String5 {
+    public static void main(String args[]){
+        String s="Hello";
+        int l=s.length();
+        for(int i=l-1;i>=0;i--){
+            System.out.print(s.charAt(i));
+        }
+    }
+}

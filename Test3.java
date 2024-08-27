dd9f9542b73cf25559e69d4a08159187c55a0f6c
Divya123solanki <solankidivya817@gmail.com>
2024-08-27 10:08:42 +0530

    Add files via upload

---
+++ Test3.java
@@ -0,0 +1,12 @@
+class Test3 {
+    public static void main(String[] args) {
+        int array_variable[]=new int[10];
+        for (int i = 0; i < 10; ++i) {
+            array_variable[i]=i/2;
+            array_variable[i]++;
+            System.out.print(array_variable[i]+" ");
+            i++;
+        }}
+
+    }
+

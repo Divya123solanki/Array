b235c2e1dc0048fe66f78990dd4f93b2a38057b7
Divya123solanki <solankidivya817@gmail.com>
2024-09-13 09:42:50 +0530

    Add files via upload

---
+++ Thread13.java
@@ -0,0 +1,13 @@
+ class Thread13 {
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            try {
+               Thread.sleep(1000);
+               System.out.println(i); 
+            }
+             catch(InterruptedException e) {
+                System.out.print(e);
+            }
+        }
+    }
+}
